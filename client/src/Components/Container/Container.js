74bd6e54bdd7140a5bb30272121e2ab39ae6a676
HeatherAckerman <hla20115213@gmail.com>
2021-01-30 16:34:11 -0600

    removed transparent effect from footer and added breaks to the container

--- client/src/Components/Container/Container.js
+++ client/src/Components/Container/Container.js
@@ -4,7 +4,7 @@ import './Container.css'
 function Container() {
     return (
         <div className='containerCss'>
-            <br />
+            <br /><br/><br/>
             <h1 className="containerH1">Container</h1>
             <p className="containerP">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <br />
@@ -16,7 +16,7 @@ function Container() {
             <br />
             <h1>Container</h1>
             <p className="containerP">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <h1>Container</h1>
+            <h1>Container</h1><br/><br/><br/><br/><br/><br/>
         </div>
     );
 }
