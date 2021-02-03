b360cbfe20aff272793e5159b90cd2d0b80690ba
HeatherAckerman <hla20115213@gmail.com>
2021-02-03 13:35:59 -0600

    replaced word-break with hyphens and added What is NEMT section to Home page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../../App.css";
-import "./home.css";
+import "./Home.css";
 
 function Home() {
     return (
         <div className="containerCss">
-            <br /><br/><br/>
+            <br /><br />
             <h1>What is NEMT</h1>
-            <br/><br/><br/><br/><br/><br/>
+            <p>Many health insurance providers want to make sure that their members can make it to their necessary medical appointments regardless of their circumstances. To make this happen they offer Non-emergency Medical Transportation (NEMT) to their members who need it. A NEMT provider is the medical taxi that provides that service. This site contains an overview of how to become a NEMT provider. </p>
+            <br/><br/>
+            <h1>Why start a NEMT Company</h1>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </div>
     );
 };
